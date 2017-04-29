f36fba6ebb19a35946723d54879b02be97e28ee6
Flavia Pittet <pittet.flavia@gmail.com>
2017-04-29 11:17:26 +0200

    email banking template

---
+++ resources/views/email/banking.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Informations pour le virement bancaire</h2>
+
+        <div>
+        	Bonjour {{ $username }} !<br />
+        	Merci pour ton insription à notre LAN!
+               <br />
+                Afin de valider ton inscription, nous te demandons d'effectuer un paiement dans les 7 jours à<br />
+        	BANING INFO HERE
+         <br />
+         <br />
+
+        </div>
+
+    </body>
+</html>
