3e555be0488d9d369d272b4b7698870225908d54
Flavia Pittet <pittet.flavia@gmail.com>
2017-05-01 20:35:26 +0200

    banking paypal configuration

---
+++ resources/views/email/paypalConfirmation.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h2>Confirmation de paiement</h2>
+
+        <div>
+        	Bonjour {{ $username }} !<br />
+        	Merci pour ton insription à notre LAN!
+               <br />
+                Ton inscription est validée
+         <br />
+         L'équipe FestiGeek
+         <br />
+
+        </div>
+
+    </body>
+</html>
