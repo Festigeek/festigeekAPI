4d8635bf4549412c2e227edfd698e02e611f4a1f
Flavia Pittet <pittet.flavia@gmail.com>
2018-03-26 16:49:32 +0200

    database seeder for 2018 created - wip

---
+++ database/seeds/2018DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Event;
+use App\Order;
+use App\Product;
+use App\ProductType;
+use App\Role;
+use App\Configuration;
+use App\Team;
+use App\User;
+use App\PaymentType;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+
+class 2018DatabaseSeeder extends Seeder
+{
+
+}
