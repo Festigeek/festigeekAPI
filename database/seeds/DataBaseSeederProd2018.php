b45326d1449937f9880a255700b24f313cac1e80
Flavia Pittet <pittet.flavia@gmail.com>
2018-04-06 19:23:31 +0200

    db seeder joueur libre

--- database/seeds/DataBaseSeederProd2018.php
+++ database/seeds/DataBaseSeederProd2018.php
@@ -31,7 +31,7 @@ class DataBaseSeederProd2018 extends Seeder
         $p9 = Product::create(['name' => 'Counter-Strike: GO', 'description' => 'Inscription CS:GO 2018.', 'price' => '20.00', 'quantity_max' => '42', 'sold' => '0', 'event_id' => 2, 'product_type_id' => 1, 'need_team'=>1]);
         $p10 = Product::create(['name' => 'Hearthstone', 'description' => 'Place joueur Hearthstone  2018.', 'price' => '20.00', 'quantity_max' => '12', 'sold' => '0', 'event_id' => 2, 'product_type_id' => 1, 'need_team'=>0]);
 
-        $p10 = Product::create(['name' => 'Animations', 'description' => 'Place joueur LAN 2018.', 'price' => '20.00', 'quantity_max' => '50', 'sold' => '0', 'event_id' => 2, 'product_type_id' => 1, 'need_team'=>0]);
+        $p10 = Product::create(['name' => 'Joueur Libre', 'description' => 'Place joueur LAN 2018.', 'price' => '20.00', 'quantity_max' => '50', 'sold' => '0', 'event_id' => 2, 'product_type_id' => 1, 'need_team'=>0]);
         $p11 = Product::create(['name' => 'Burger', 'description' => "Bon d'achat pour un Burger Festigeek !", 'price' => '14.00', 'product_type_id' => 2, 'sold' => '0', 'event_id' => 2, 'need_team'=>0]);
         $p12 = Product::create(['name' => 'Petit-déjeuner', 'description' => "Bon d'achat pour un petit-déjeuner.", 'price' => '5.00', 'product_type_id' => 2, 'sold' => '0', 'event_id' => 2, 'need_team'=>0]);
         Product::create(['name' => 'Burger Gratuit', 'description' => "Bon pour un Burger Festigeek gratuit.", 'price' => '0.00', 'product_type_id' => 2, 'sold' => '0', 'event_id' => 2, 'need_team'=>0]);
