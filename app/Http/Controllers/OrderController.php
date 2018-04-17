e641728bcf714008ad35d3d5163873ce5d562caf
Mysteriosis <pacurty@gmail.com>
2018-04-17 14:55:45 +0200

    Log Auth

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -288,6 +288,10 @@ class OrderController extends Controller
      */
     private function manageTeam(Request $request , Order $order)
     {
+        Log::log("TEST - manageTeam");
+        Log::info(Auth::user());
+        Log::info(Auth::id());
+
         $result = ['error' => false];
         if ($request->filled('team_code')) {
             $result['team'] = Team::where('code', '=', $request->get('team_code'))->get();
@@ -296,7 +300,7 @@ class OrderController extends Controller
                 $result['infoError'] = ['error' => 'Wrong team code'];
                 return $result;
             }
-            $order->team()->attach($result['team']->id, ['captain' => false, 'user_id' => Auth::id()]);
+            $order->team()->attach($result['team']->id, ['captain' => false, 'user_id' => $request->user()->id]);
         }
         else if ($request->filled('team')) {
             $collisions = Team::where('alias', '=', Team::generateAlias($request->get('team')))->get();
@@ -310,7 +314,7 @@ class OrderController extends Controller
             }
             $result['team'] = Team::create(['name' => $request->get('team')]);
             $result['team']->save();
-            $order->team()->attach($result['team']->id, ['captain' => true, 'user_id' => Auth::id()]);
+            $order->team()->attach($result['team']->id, ['captain' => true, 'user_id' => $request->user()->id]);
             $order->save();
         }
         else {
@@ -362,6 +366,10 @@ class OrderController extends Controller
 
         $user = $order->user()->first();
         $team = $order->team()->first();
+
+        Log::log("TEST - bankTransferPayment");
+        Log::info(Auth::user());
+        Log::info(Auth::id());
 
         if(!is_null($team) && $team->captain->id == Auth::id())
             Mail::to($user->email, $user->username)->send(new TeamOwnerMail($user, $team));
@@ -453,7 +461,7 @@ class OrderController extends Controller
             if (is_null($event_id))
                 return response()->json(['error' => 'add event id']);
 
-            if (is_null(Auth::user()))
+            if (!Auth::check())
                 return response()->json(['error' => 'Authentication error'], 401);
 
             if (Auth::user()->orders()->where('event_id', $request->get('event_id'))->get()->isNotEmpty())
@@ -493,6 +501,10 @@ class OrderController extends Controller
             // Check if free burger (contest) have been added by forging request
             if ($products->pluck('product_id')->contains(self::FREE_BURGER_ID))
                 return response()->json(['error' => 'Go fuck yourself'], 418);
+
+            Log::log("TEST - create");
+            Log::info(Auth::user());
+            Log::info(Auth::id());
 
             // Create order
             $order = Order::create([
